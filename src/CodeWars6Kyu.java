fb6836828a931f089fa4b9a3c0cd6e01d7c608a7
jespertin <jespertinell@gmail.com>
2023-12-19 22:27:12 +0100

    isPangram

--- src/CodeWars6Kyu.java
+++ src/CodeWars6Kyu.java
@@ -449,4 +449,25 @@ public class CodeWars6Kyu {
     }
 
 
+    /**
+     * A pangram is a sentence that contains every single letter of the alphabet at least once.
+     * For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters
+     * A-Z at least once (case is irrelevant).
+     * Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+     * */
+    public static boolean isPangram(String sentence) {
+        sentence = sentence.toLowerCase();
+
+        Set<Character> letters = new HashSet<>();
+
+        for (char ch : sentence.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                letters.add(ch);
+            }
+        }
+
+        return letters.size() == 26;
+    }
+
+
 }
